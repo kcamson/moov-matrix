c849d5e620826e7e3030a388e392276c3e245f57
MaxBreslin <pokemax209@gmail.com>
2024-05-19 14:00:35 -0700

    BACKUP

---
+++ .backup/nightscout_secrets.py
@@ -0,0 +1,7 @@
+secrets = {
+    'ssid' : 'HANSELMAN-MESH',
+    'password' : 'cheeseman',
+    'timezone' : "America/Los_Angeles", 
+    'token' : "api-98a89f7eb6311cf5",
+    'nightscout' : "https://hanselsugar.herokuapp.com"
+    } 
